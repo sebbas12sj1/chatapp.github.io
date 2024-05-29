29843bd4d6e9bc7d348c07f1541652662c77924c
sebbas12sj1 <sebastianbarrera921@gmail.com>
2024-05-29 11:04:37 -0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+
+// Tu configuración de Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAnFHdXilN26qS_ljpC2YfUgE98NMASZYY",
+  authDomain: "loginfirebase-1f5fb.firebaseapp.com",
+  projectId: "loginfirebase-1f5fb",
+  storageBucket: "loginfirebase-1f5fb.appspot.com",
+  messagingSenderId: "596077454191",
+  appId: "1:596077454191:web:c70370d831f52600bc7e9a",
+  measurementId: "G-KB3FDTGZ2J"
+};
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
